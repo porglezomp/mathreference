09490fc9cb88f431a63d8b3f98f1f157beb6e458
Caleb Jones <bionicle.dragon@gmail.com>
2014-09-18 21:08:30 -0400

    Cleanup imgdownloader.py
    Fix indentation
    Cleanup long lines
    - Split multistep processes into multiple lines
    Split imports onto multiple lines (pep8)
    Check pep8 compliance (passes)

--- imgdownloader.py
+++ imgdownloader.py
@@ -1,25 +1,45 @@
 #!/usr/bin/env python
 
-import urllib, sys, re, string, hashlib, base64, os, subprocess
+import urllib
+import sys
+import re
+import string
+import hashlib
+import base64
+import os
+import subprocess
 
 if not os.path.isdir("images/"):
-	print "Created 'images/' directory"
-	os.mkdir("images/")
+    print "Created 'images/' directory"
+    os.mkdir("images/")
 NO_IMAGEMAGICK = False
 fr = ["\\", " ", "^", "{", "}", "(", ")", "[", "]", "&"]
 to = ["",   "",  "",  "",  "",  "",  "",  "",  "",  ""]
-regex = re.compile('<img ?(style=".*?")? src="http://(?P<url>.*?)\?(?P<file>.*?)">')
+regex = re.compile('<img ?(style=".*?")? '
+                   '"src="http://(?P<url>.*?)\?(?P<file>.*?)">')
 a = sys.stdin.readline()
 while a:
-	for item in regex.finditer(a):
-		fname = "images/"+base64.b64encode(hashlib.sha224(item.group('file')).digest(), "ab")[:15]+".gif"
-		if not os.path.isfile(fname):
-			urllib.urlretrieve("http://"+item.group('url')+"?"+item.group('file'), fname)
-		try:
-			if not NO_IMAGEMAGICK: subprocess.Popen(["convert", fname, "-contrast-stretch", "100%", fname])
-		except:
-			NO_IMAGEMAGICK = True
-			sys.stderr.writelines("ERROR: It appears that ImageMagick is not installed, contrast may suffer in equations.")
-		a = a.replace("http://"+item.group('url')+"?"+item.group('file'), fname)
-	sys.stdout.write(a)
-	a = sys.stdin.readline()
+    for item in regex.finditer(a):
+        shaname = hashlib.sha224(item.group('file')).digest(),
+        b64name = base64.b64encode(shaname, "ab")
+        fname = "images/" + b64name[:15] + ".gif"
+        if not os.path.isfile(fname):
+            url = "http://" + item.group('url') + "?" + item.group('file')
+            urllib.urlretrieve(url, fname)
+        try:
+            if not NO_IMAGEMAGICK:
+                subprocess.Popen(["convert",
+                                  fname,
+                                  "-contrast-stretch",
+                                  "100%",
+                                  fname])
+        except:
+            NO_IMAGEMAGICK = True
+            msg = "ERROR: It appears taht ImageMagick is not installed,"\
+                  "contrast may suffer in equations."
+            sys.stderr.writelines(msg)
+        item_url = item.group('url')
+        item_file = item.group('file')
+        a = a.replace("http://" + item_url + "?" + item_file, fname)
+    sys.stdout.write(a)
+    a = sys.stdin.readline()
